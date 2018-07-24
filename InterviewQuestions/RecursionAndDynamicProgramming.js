3a8245e6a7cf739f8dc3b42f617dab844ca1b8a9
Stephen Carico <stephen.r.carico@gmail.com>
2018-07-23 18:51:23 -0700

    added optimized 8.1 solution

--- InterviewQuestions/RecursionAndDynamicProgramming.js
+++ InterviewQuestions/RecursionAndDynamicProgramming.js
@@ -6,20 +6,35 @@
 // ---------- 8.1 TRIPLE STEP ---------- //
 // //////////////////////////////////// // 
 
-function numWays(N) {
-  let answer = 0;
-  let recurse = function(number) {
-    if (number === 0) {
-      answer++;
-    } else if (number > 0) {
-      recurse(number - 1);
-      recurse(number - 2);
-      recurse(number - 3);
-    }
+// BRUTE FORCE ~ O(N)
+// function numWays(N) {
+//   let answer = 0;
+//   let recurse = function(number) {
+//     if (number === 0) {
+//       answer++;
+//     } else if (number > 0) {
+//       recurse(number - 1);
+//       recurse(number - 2);
+//       recurse(number - 3);
+//     }
+//   }
+//   recurse(N)
+//   return answer;
+// }
+
+// MEMOIZATION ~ O(LOG N)
+var numWays = function(n, memo) {
+  if (n < 0) {
+    return 0
+  } else if (n === 0) {
+    return 1
+  } else if (memo[n] > -1) {
+    return memo[n];
+  } else {
+    memo[n] = numWays(n-1, memo) + numWays(n-2, memo) + numWays(n-3, memo)
+    return memo[n]
   }
-  recurse(N)
-  return answer;
-}
+};
 
 // console.log(numWays(3))
 
